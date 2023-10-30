b05a1064a129419437718d450f469f6ecc2a6b64
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2023-10-30 22:21:49 +0530

    added email

--- my-app/src/components/Email.js
+++ my-app/src/components/Email.js
@@ -3,7 +3,7 @@ function Email(){
         <h1>Get In Touch</h1>
         <h1>Ready to Work.</h1>
         <p>I'm looking for opportunities for using my skills</p>
-        <button class="button"><a class="btn" href="mailto:name@email.com">CONTACT ME</a></button>
+        <button class="button"><a class="btn" href="mailto:sreejithn2002@gmail.com">CONTACT ME</a></button>
     </>
 }
 
