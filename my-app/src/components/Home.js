9707cd36e3346b1aacbb3c926f83bf45fad0fd5d
Sreejith P <99952612+Sreejith2@users.noreply.github.com>
2023-11-04 15:24:10 +0530

    Update Home.js

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 function Home() {
+  const [text] = useTypewriter({words:['Backend Developer','Student','React Developer'],loop:{}});
   return (
     <Container id="home" className='cont'>
       <Row className="mt-5">
         <Col className="text-center">
           <h1><span className='intro'>Hi I am</span><span className='username'> Sreejith</span></h1>
-          <h2 className='about'>A BackEnd Developer</h2>
+          <h2 className='about'>A <span style={{color:"orange"}}>{text}</span><Cursor/></h2>
           <p className='desc'>I am deeply passionate about web development, with a special love for backend development. Crafting robust, efficient systems and databases that power the digital world is where my heart truly lies. I find immense satisfaction in building the unseen backbone of websites and applications.</p>
         </Col>
       </Row>
